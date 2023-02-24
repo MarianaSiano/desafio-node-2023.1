09de400a33b023f71440c7fb2916316029abd110
Mariana <maryhsiano@hotmail.com>
2023-02-24 19:22:11 -0300

    Erros corrigidos

--- Desafio-Node/src/server.ts
+++ Desafio-Node/src/server.ts
@@ -1,14 +1,9 @@
-import express from 'express'
+import express = require("express");
 
 const app = express();
 
 app.use(express.json());
 
-
-app.get('/', (req, res) => {
-  console.log("GET /");
-  res.status(200).send("Hello World");
-});
 
 app.listen('3333', () => {
   console.log("O servidor foi inicializado em http://localhost:3333");
