4c74f2e11709fbedfcdff4fd2f8ec4389fba265c
Mariana <maryhsiano@hotmail.com>
2023-03-04 20:20:23 -0300

    import do service feito

--- Desafio-Node/src/controllers/restaurante/CreateRestauranteController.ts
+++ Desafio-Node/src/controllers/restaurante/CreateRestauranteController.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from "express";
+import { CreateRestauranteService } from "../../services/restaurante/CreateRestauranteService";
 
 class CreateRestauranteController {
     async handle(req: Request, res: Response) {
         const { nome, email, senha, categoria, cidade, endereco, telefone } = req.body;
+        
         return res.json({ok: true});
     }
 }
