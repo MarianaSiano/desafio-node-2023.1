9186209733a0ff72bacbe933172e4a69a0594db4
Mariana <maryhsiano@hotmail.com>
2023-03-07 11:51:54 -0300

    Cadastrando Restaurantes

--- Desafio-Node/src/services/restaurante/CreateRestauranteService.ts
+++ Desafio-Node/src/services/restaurante/CreateRestauranteService.ts
@@ -1,3 +1,5 @@
+import prismaClient from "../../prisma";
+
 interface RestauranteRequest {
     nome: string;
     email: string;
@@ -10,8 +12,44 @@ interface RestauranteRequest {
 
 class CreateRestauranteService {
     async execute({nome, email, senha, categoria, cidade, endereco, telefone}: RestauranteRequest) {
-        console.log(nome);
-        return {nome: nome};
+        //Verificar se mandou o email
+        if(!email) {
+            throw new Error("Email incorreto");
+        }
+
+        //verificar se esse email já está cadastrado na plataforma
+        const restauranteAlreadyExists = await prismaClient.restaurante.findFirst({
+            where: {
+                email: email
+            }
+        });
+
+        if(restauranteAlreadyExists) {
+            throw new Error("Restaurante já cadastrado");
+        }
+
+        const restaurante = await prismaClient.restaurante.create({
+            data: {
+                nome: nome,
+                email: email,
+                senha: senha,
+                categoria: categoria,
+                cidade: cidade,
+                endereco: endereco,
+                telefone: telefone
+            },
+            select: {
+                id: true,
+                nome: true,
+                email: true,
+                categoria: true,
+                cidade: true,
+                endereco: true,
+                telefone: true
+            }
+        });
+
+        return restaurante;
     }
 }
 
