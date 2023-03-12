efced077407560e5c713f648c0368f832d68e0cc
Mariana <maryhsiano@hotmail.com>
2023-03-12 20:14:25 -0300

    'CreateProdutoController' terminado com as informacoes necessarias

--- Desafio-Node/src/controllers/produto/CreateProdutoController.ts
+++ Desafio-Node/src/controllers/produto/CreateProdutoController.ts
@@ -1,5 +1,14 @@
 import { Request, Response } from 'express';
+import { CreateProdutoService } from '../../services/produto/CreateProdutoService';
 
-class CreateProdutoController {}
+class CreateProdutoController {
+    async handle(req: Request, res: Response) {
+        const { nome, descricao, quantidade, preco, categoria, restaurante_id } = req.body;
+        const createProdutoController = new CreateProdutoService();
+        const produto = createProdutoController.execute(req.body);
+
+        return res.json(produto);
+    }
+}
 
 export { CreateProdutoController };
