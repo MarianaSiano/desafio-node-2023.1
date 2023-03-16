0343394dfaba3c28acb7709da9664ac0fc916957
Mariana <maryhsiano@hotmail.com>
2023-03-16 20:41:50 -0300

    Comecando o Controller do Delete

---
+++ Desafio-Node/src/controllers/restaurante/DeleteRestauranteController.ts
@@ -0,0 +1,6 @@
+import {  Request, Response } from 'express';
+import { DeleteRestauranteService } from '../../services/restaurante/DeleteRestauranteService';
+
+class DeleteRestauranteController {}
+
+export { DeleteRestauranteController}
