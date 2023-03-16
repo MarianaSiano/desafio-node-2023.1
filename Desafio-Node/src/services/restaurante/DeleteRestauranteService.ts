39fc51e3565d40dfe31d3bfd6e7320114f6cd331
Mariana <maryhsiano@hotmail.com>
2023-03-16 20:37:16 -0300

    'DeleteRestauranteService.ts' terminado

--- Desafio-Node/src/services/restaurante/DeleteRestauranteService.ts
+++ Desafio-Node/src/services/restaurante/DeleteRestauranteService.ts
@@ -1,5 +1,23 @@
 import prismaClient from "../../prisma";
 
-class DeleteRestauranteService {}
+class DeleteRestauranteService {
+    async execute(id: string) {
+        const restaurante = await prismaClient.restaurante.delete({
+            where: {
+                id: parseInt(id)
+            }
+        });
+
+        if(!restaurante) {
+            throw new Error("Restaurante não encontrado");
+        }
+
+        await prismaClient.restaurante.delete({
+            where: {
+                id: parseInt(id)
+            }
+        });
+    }
+}
 
 export { DeleteRestauranteService }
