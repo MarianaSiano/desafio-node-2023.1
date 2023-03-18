e2d9fccf0ce1d39d81548a7091aedb7da185032c
Mariana <maryhsiano@hotmail.com>
2023-03-18 01:06:56 -0300

    Read de um produto feito

--- Desafio-Node/src/services/produto/ReadProdutoService.ts
+++ Desafio-Node/src/services/produto/ReadProdutoService.ts
@@ -1,5 +1,20 @@
 import prismaClient from "../../prisma";
 
-class ReadProdutoService {}
+class ReadProdutoService {
+    async execute(produto_id: string) {
+        const produto = await prismaClient.produto.findFirst({
+            where: {
+                id: parseInt(produto_id)
+            },
+            select: {
+                id: true,
+                nome: true,
+                descricao: true,
+                preco: true,
+                categoria: true
+            }
+        });
+    }
+}
 
 export { ReadProdutoService };
