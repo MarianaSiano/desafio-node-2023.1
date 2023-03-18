5f213de6b731462df1881a1df60d3c10ee694f13
Mariana <maryhsiano@hotmail.com>
2023-03-18 12:49:53 -0300

    Corrigindo a rota de visualizar todos os produtos

--- Desafio-Node/src/routes.ts
+++ Desafio-Node/src/routes.ts
@@ -3,12 +3,13 @@ import { Router } from "express";
 //Controllers
 import { CreateRestauranteController } from "./controllers/restaurante/CreateRestauranteController"; //Controller criar Restaurante
 import { AuthRestauranteController } from "./controllers/restaurante/AuthRestauranteController"; //Controller autenticar Restaurante
-import { ReadRestauranteController } from "./controllers/restaurante/ReadRestauranteController"; //Controller ler Restaurante
-import { ReadAllRestaurantesController } from "./controllers/restaurante/ReadAllRestaurantesController"; //Controller ler todos os Restaurantes
+import { ReadRestauranteController } from "./controllers/restaurante/ReadRestauranteController"; //Controller visualizar Restaurante
+import { ReadAllRestaurantesController } from "./controllers/restaurante/ReadAllRestaurantesController"; //Controller visualizar todos os Restaurantes
 import { UpdateRestauranteController } from "./controllers/restaurante/UpdateRestauranteController"; //Controller atualizar Restaurante
 import { DeleteRestauranteController } from "./controllers/restaurante/DeleteRestauranteController"; //Controller deletar Restaurante
 import { CreateProdutoController } from "./controllers/produto/CreateProdutoController"; //Controller criar Produto
-import { ReadProdutoController } from "./controllers/produto/ReadProdutoController"; //Controller ler Produto
+import { ReadProdutoController } from "./controllers/produto/ReadProdutoController"; //Controller visualizar Produto
+import { ReadAllProdutosController } from "./controllers/produto/ReadAllProdutosController"; //Controller visualizar todos os Produtos
 
 //Middlewares
 import { isAuthenticated } from "./middlewares/isAuthenticated"; //Autenticação
@@ -30,7 +31,7 @@ router.get("/restaurantes", new ReadAllRestaurantesController().handle); //Rota 
 
 //Produto
 router.get("/detalheProduto/:id", new ReadProdutoController().handle); //Rota de visualizar um produto
-router.get("/produtos", new ReadAllRestaurantesController().handle); //Rota de visualizar todos os produtos
+router.get("/produtos", new ReadAllProdutosController().handle); //Rota de visualizar todos os produtos
 
 //put
 //Restaurante
