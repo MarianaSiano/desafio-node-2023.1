6c560da9321b1dd8475425d97340e263568d0ab2
Mariana <maryhsiano@hotmail.com>
2023-03-18 00:58:55 -0300

    Deixando registrado o erro pelo qual o restaurante do id 4 nao eh deletado

--- Desafio-Node/src/controllers/restaurante/DeleteRestauranteController.ts
+++ Desafio-Node/src/controllers/restaurante/DeleteRestauranteController.ts
@@ -6,15 +6,16 @@ class DeleteRestauranteController {
         const id = parseInt(req.params.id);
         const resultado = await DeleteRestauranteService.deletarRecurso({id});
         
-        // try{
-        //     if(!resultado) {
-        //         throw new Error("Não foi possível deletar o restaurante");
-        //     }
-        // }catch(err) {
-        //     return res.status(400).json({
-        //         message: err.message
-        //     })
-        // }
+        try{
+            if(!resultado) {
+                throw new Error("Não foi possível deletar o restaurante");
+            }
+        }catch(err) {
+            return res.status(400).json({
+                message: err.message
+            })
+        } //Está dando problema para deletar o restaurante com id 4.
+
         res.send(resultado)
     }
 }
