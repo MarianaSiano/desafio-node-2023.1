f8571957c673f65ff03a5fa5683a246cf3d2621f
Mariana <maryhsiano@hotmail.com>
2023-03-19 16:11:30 -0300

    Rotas atualizadas

--- Desafio-Node/src/routes.ts
+++ Desafio-Node/src/routes.ts
@@ -28,51 +28,29 @@ import { isAuthenticated } from "./middlewares/isAuthenticated"; //Autenticaçã
 
 const router = Router();
 
-//post
 //Restaurante
 router.post("/restaurante/create", new CreateRestauranteController().handle); //Rota de Criar Restaurante
-router.post("/login", new AuthRestauranteController().handle); //Rota de Login Restaurante
-
-//Produto
-router.post("/produto/create", isAuthenticated, new CreateProdutoController().handle); //Rota de Criar Produto
-
-//Pedido
-router.post("/pedido/create", isAuthenticated, new CreatePedidoController().handle); //Rota de Criar Pedido
-
-//get
-//Restaurante
 router.get("/restaurante/detalhe/:id", isAuthenticated, new ReadRestauranteController().handle); //Rota de visualizar um Restaurante enquanto está logado
 router.get("/restaurantes/detalhes", new ReadAllRestaurantesController().handle); //Rota de visualizar todos os resstaurante, não precisa estar logado
-
-//Produto
-router.get("/produto/detalhe/:id", new ReadProdutoController().handle); //Rota de visualizar um produto
-router.get("/produtos/detalhes", new ReadAllProdutosController().handle); //Rota de visualizar todos os produtos
-
-//Pedido
-router.get("/pedido/detalhe/:id", isAuthenticated, new ReadPedidosController().handle); //Rota de visualizar um pedido
-router.get("/pedidos/detalhes", isAuthenticated, new ReadAllPedidosController().handle); //Rota de visualizar todos os pedidos
-
-//put
-//Restaurante
-//funciona apenas com o /:id no final da rota e nao com o /10, por exemplo
-router.put("/restaurante/update/:id", isAuthenticated, new UpdateRestauranteController().handle); //Rota de atualizar Restaurante
-
-//Produto
-//funciona apenas com o /:id no final da rota e nao com o /10, por exemplo
-router.put("/produto/update/:id", isAuthenticated, new UpdateProdutoController().handle); //Rota de atualizar Produto
-
-//Pedido
-//funciona apenas com o /:id no final da rota e nao com o /10, por exemplo
-router.put("/pedido/update/:id", isAuthenticated, new UpdatePedidoController().handle); //Rota de atualizar Pedido
-
-//delete
-//Restaurante
+router.put("/restaurante/update/:id", isAuthenticated, new UpdateRestauranteController().handle); //Rota de atualizar Restaurante, funciona apenas com o /:id no final da rota e nao com o /10, por exemplo
 router.delete("/restaurante/delete/:id", isAuthenticated, new DeleteRestauranteController().handle); //Rota de deletar Restaurante
 
 //Produto
+router.post("/produto/create", isAuthenticated, new CreateProdutoController().handle); //Rota de Criar Produto
+router.get("/produto/detalhe/:id", new ReadProdutoController().handle); //Rota de visualizar um produto
+router.get("/produtos/detalhes", new ReadAllProdutosController().handle); //Rota de visualizar todos os produtos
+router.put("/produto/update/:id", isAuthenticated, new UpdateProdutoController().handle); //Rota de atualizar Produto, funciona apenas com o /:id no final da rota e nao com o /10, por exemplo
 router.delete("/produto/delete/:id", isAuthenticated, new DeleteProdutoController().handle); //Rota de deletar Produto
 
 //Pedido
+router.post("/pedido/create", isAuthenticated, new CreatePedidoController().handle); //Rota de Criar Pedido
+router.get("/pedido/detalhe/:id", isAuthenticated, new ReadPedidosController().handle); //Rota de visualizar um pedido
+router.get("/pedidos/detalhes", isAuthenticated, new ReadAllPedidosController().handle); //Rota de visualizar todos os pedidos
+router.put("/pedido/update/:id", isAuthenticated, new UpdatePedidoController().handle); //Rota de atualizar Pedido, funciona apenas com o /:id no final da rota e nao com o /10, por exemplo
 router.delete("/pedido/delete/:id", isAuthenticated, new DeletePedidoController().handle); //Rota de deletar Pedido
+
+//Login
+router.post("/login", new AuthRestauranteController().handle); //Rota de Login
+
 
 export { router };
